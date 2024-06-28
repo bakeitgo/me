5779e32e5c2f4f2bb7c86c02efddebc401098bc7
wojesky <m.lojewski1998@gmail.com>
2024-06-28 16:51:34 +0200

    fix queue

--- posts/data-structures/structures/queue/queue.go
+++ posts/data-structures/structures/queue/queue.go
@@ -1,41 +1,39 @@
 package queue
 
-type node[T] struct {
+type node[T any] struct {
 	data T
-	next *Node[T]
+	next *node[T]
 }
 
-type queue[T] struct {
+type queue[T any] struct {
 	tail *node[T]
 	head *node[T]
-	len int
+	len  int
 }
 
-
-
-func (q *queue) Peek() (firstItem T) {
-	firstItem = s.head.data
+func (q *queue[T]) Peek() (firstItem T) {
+	firstItem = q.head.data
 	return
 }
 
-func (q *queue) Deque() (dequed T)  {
+func (q *queue[T]) Deque() (dequed T) {
 	dequed, q.head = q.head.data, q.head.next
 	q.len--
 	return
 }
 
-func (q *queue) Enqueue(item T) {
-	node := &node[T]{item}
+func (q *queue[T]) Enqueue(item T) {
+	node := &node[T]{item, nil}
 	if q.len == 0 {
 		q.tail = node
 		q.head = node
 		return
-	} 
+	}
 	q.tail.next = node
 	q.tail = node
 	defer func() { q.len++ }()
 }
 
-func (q *queue) NewQueue() *queue {
-	return &queue{}
+func NewQueue[T any]() *queue[T] {
+	return &queue[T]{}
 }
